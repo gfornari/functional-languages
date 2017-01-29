49907f044b3de8d1719cfe08f4bc7a3a2c3e1086
Giacomo Fornari <fornarigiacomo@gmail.com>
2017-01-29 15:50:51 +0100

    Change putStrLn show to print

--- ziplist/Main.hs
+++ ziplist/Main.hs
@@ -5,15 +5,15 @@ import ZipList
 main :: IO ()
 main = do
     putStrLn "Showing the Applicative ZipList:"
-    putStrLn (show (Z [(+), (*), (/)] <*> Z [2,4,8] <*> Z [1,3]))
+    print $ Z [(+), (*), (/)] <*> Z [2,4,8] <*> Z [1,3]
     
     putStrLn "Showing the exercise a:"
-    putStrLn (show (zipA [1,2,3] [4,5,6,10] [7,8,9]))
-    putStrLn (show (zipA [1,2,3] [] [7,8,9]))
+    print $ zipA [1,2,3] [4,5,6,10] [7,8,9]
+    print $ zipA [1,2,3] [] [7,8,9]
     
     putStrLn "Showing the exercise b:"
-    putStrLn (show (zipB [1,2,3] [4,5,6,8] [7,8,9]))
-    putStrLn (show (zipB [1,2,3] [] [7,8,9]))
+    print $ zipB [1,2,3] [4,5,6,8] [7,8,9]
+    print $ zipB [1,2,3] [] [7,8,9]
 
     putStrLn "Showing the exercise C:"
-    putStrLn (show (zipC [[1,2],[3,4],[5,6]]))
+    print $ zipC [[1,2],[3,4],[5,6]]
